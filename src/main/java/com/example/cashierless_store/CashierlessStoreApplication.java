800323523d597d656f4421d22ac4df4941186b75
yoon <yoon@yoonui-MacBookAir.local>
2022-07-15 02:35:26 +0900

    test

---
+++ src/main/java/com/example/cashierless_store/CashierlessStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashierless_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashierlessStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashierlessStoreApplication.class, args);
+    }
+
+}
